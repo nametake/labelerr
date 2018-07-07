bb78dc9a8078b6366d70a11d0e3073b4b60cafb2
nametake <nametake.kyarabuki@gmail.com>
2018-07-07 14:41:26 +0900

    Add subError struct

--- lvlerr.go
+++ lvlerr.go
@@ -1,1 +1,34 @@
 package lvlerr
+
+import "fmt"
+
+type causer interface {
+	Cause() error
+}
+
+type leveler interface {
+	Level() string
+}
+
+var (
+	_ error   = (*lvlError)(nil)
+	_ causer  = (*lvlError)(nil)
+	_ leveler = (*lvlError)(nil)
+)
+
+type lvlError struct {
+	err   error
+	level string
+}
+
+func (l *lvlError) Error() string {
+	return fmt.Sprintf("%s: %s", l.level, l.err)
+}
+
+func (l *lvlError) Cause() error {
+	return l.err
+}
+
+func (l *lvlError) Level() string {
+	return l.level
+}
