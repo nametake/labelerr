46774db8076e4207581aa7a080365d0cfccbcc0b
nametake <nametake.kyarabuki@gmail.com>
2018-08-04 14:15:35 +0900

    Add Wrap test

--- labelerr_test.go
+++ labelerr_test.go
@@ -1,6 +1,10 @@
 package labelerr
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/pkg/errors"
+)
 
 func TestNew(t *testing.T) {
 	tests := []struct {
@@ -22,7 +26,7 @@ func TestNew(t *testing.T) {
 			want:  "warning: ",
 		},
 		{
-			name:  "empty level",
+			name:  "empty label",
 			msg:   "error message",
 			label: "",
 			want:  ": error message",
@@ -40,5 +44,40 @@ func TestNew(t *testing.T) {
 				t.Errorf("New.Error(): got: %v, want %v", got, tt.want)
 			}
 		})
+	}
+}
+
+func TestWrap(t *testing.T) {
+	tests := []struct {
+		name  string
+		err   error
+		label string
+		want  string
+	}{
+		{
+			name:  "simple",
+			err:   errors.New("error"),
+			label: "label",
+			want:  "label: error",
+		},
+		{
+			name:  "empty label",
+			err:   errors.New("error"),
+			label: "",
+			want:  ": error",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Wrap(tt.err, tt.label).Error(); got != tt.want {
+				t.Errorf("Wrap.Error(): got: %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestWrapNil(t *testing.T) {
+	if got := Wrap(nil, "no error"); got != nil {
+		t.Errorf("Wrap(nil, \"no error\"): got: %v, want nil", got)
 	}
 }
