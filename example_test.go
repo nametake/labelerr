a32b5515e7b919da621dd1e99ee2957ee4cc4911
nametake <nametake.kyarabuki@gmail.com>
2018-08-16 01:47:23 +0900

    Update example

--- example_test.go
+++ example_test.go
@@ -7,20 +7,33 @@ import (
 	"github.com/pkg/errors"
 )
 
-func ExampleLabel() {
-	err := errors.New("cause")
+func cause() error {
+	return errors.New("cause")
+}
 
-	err = errors.Wrap(err, "first")
+func first() error {
+	err := cause()
 	err = labelerr.Wrap(err, "warning")
-	err = errors.Wrap(err, "second")
-	err = errors.Wrap(err, "third")
+	return errors.Wrap(err, "first")
+
+	// one line:
+	// return labelerr.WithMessage(err, "warning", "first")
+}
+
+func second() error {
+	err := first()
+	return errors.Wrap(err, "second")
+}
+
+func Example() {
+	err := second()
 
 	fmt.Println(err)
 	fmt.Println(labelerr.Label(err))
 	fmt.Println(errors.Cause(err))
 
 	// Output:
-	// third: second: warning: first: cause
+	// second: first: warning: cause
 	// warning
 	// cause
 }
